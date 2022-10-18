f6a09203bca73c7c106ba7a4e443a5180154d213
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-18 14:52:17 +0300

    fix bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,13 +10,13 @@ import sys
 
 
 if __name__ == '__main__':
-    args = int(sys.argv[1])
+    args = sys.argv[1]
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                         .format(args))
     todo = requests.get('https://jsonplaceholder.typicode.com/todos')
     data_todo = todo.json()
     name = user.json().get('name')
-    new_list = [i for i in data_todo if i.get("userId") == args]
+    new_list = [i for i in data_todo if i.get("userId") == int(args)]
     total_task = len(new_list)
     done = [i for i in new_list if i.get('completed') is True]
     done_task = len(done)
