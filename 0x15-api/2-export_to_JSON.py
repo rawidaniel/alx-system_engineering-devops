2f6c6b345de6e97a51d0ec1dc59c0d5676dab6b4
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-19 07:23:44 +0300

    convert specified user data to json formata

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(userId))
+    username = user.json().get('username')
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+    new_list = [i for i in todo.json() if i.get("userId") == int(userId)]
+    formated_list = []
+    for entery in new_list:
+        if entery.get('userId') == int(userId):
+            new_dict = {}
+            new_dict['task'] = entery['title']
+            new_dict['completed'] = entery['completed']
+            new_dict['username'] = username
+            formated_list.append(new_dict)
+    dict = {f'{userId}': formated_list}
+    with open(f"{userId}.json", 'w') as write_file:
+        json.dump(dict, write_file)
