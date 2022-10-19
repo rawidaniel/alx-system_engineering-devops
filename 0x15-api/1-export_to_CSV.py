2ac6e745aaeb9fea16fc9744f42de0c9e1fe63be
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-19 07:51:08 +0300

    fix bug

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
                         .format(userId))
     username = user.json().get('username')
     todo = requests.get('https://jsonplaceholder.typicode.com/todos')
-    new_list = [i for i in todo.json() if i.get("userId") == int(userId)]
     formated_list = []
-    for entery in new_list:
-        entery.pop('id')
-        entery["username"] = username
-        formated_list.append(entery)
+    for entery in todo.json():
+        if entery.get("userId") == int(userId):
+            entery.pop('id')
+            entery["username"] = username
+            formated_list.append(entery)
     fields = ["userId", "username", "completed", "title"]
     with open(f'{userId}.csv', 'w') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fields,
